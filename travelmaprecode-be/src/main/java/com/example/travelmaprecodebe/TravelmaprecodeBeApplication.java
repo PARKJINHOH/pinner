c8065b4b9a33163870079b7ac069f09274a8308b
Jino <adg0609@naver.com>
2022-07-16 17:38:26 +0900

    be init

---
+++ travelmaprecode-be/src/main/java/com/example/travelmaprecodebe/TravelmaprecodeBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.travelmaprecodebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelmaprecodeBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelmaprecodeBeApplication.class, args);
+    }
+
+}
---
+++ travelmaprecode-be/src/test/java/com/example/travelmaprecodebe/TravelmaprecodeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travelmaprecodebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelmaprecodeBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
