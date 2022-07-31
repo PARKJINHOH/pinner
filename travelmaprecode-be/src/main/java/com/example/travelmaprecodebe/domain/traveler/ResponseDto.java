f53736770f306e7b520dca0b0943f095330a5c82
cmsong <cmsong@shinasys.com>
2022-07-31 23:43:07 +0900

    [be] ResponseDTO Empty한 값은 JSON 직렬화 하지 않도록 변경

--- travelmaprecode-be/src/main/java/com/example/travelmaprecodebe/domain/traveler/ResponseDto.java
+++ travelmaprecode-be/src/main/java/com/example/travelmaprecodebe/domain/traveler/ResponseDto.java
@@ -1,12 +1,22 @@
 package com.example.travelmaprecodebe.domain.traveler;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class ResponseDto {
     Map<String, Object> data = new HashMap<>();
     String message;
+
+    public ResponseDto(String message) {
+        this.message = message;
+    }
+
+    public ResponseDto(Map<String, Object> data) {
+        this.data = data;
+    }
 }
