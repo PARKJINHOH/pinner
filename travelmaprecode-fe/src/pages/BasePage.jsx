b5c73e07046f575d7ed014c902801ecb4c79848f
cmsong <dlcmaldmlzldxkdlrj@gmail.com>
2022-09-01 01:08:32 +0900

    [fe] BasePage에서 auth 버튼 제거

--- travelmaprecode-fe/src/pages/BasePage.jsx
+++ travelmaprecode-fe/src/pages/BasePage.jsx
@@ -6,19 +6,11 @@ import { useRecoilValue, useSetRecoilState } from 'recoil';
 import LoginModal from '../components/modals/LoginModal';
 import RegisterModal from '../components/modals/RegisterModal';
 
-import './BasePage.css';
-
 import { ModalVisibility, modalVisibilityState } from '../states/modal';
 import { isLoggedInState, useDoLogout } from '../states/traveler';
 
 
 export default function BasePage() {
-    const isLoggedIn = useRecoilValue(isLoggedInState);
-    const doLogout = useDoLogout();
-
-    const setModalVisibility = useSetRecoilState(modalVisibilityState);
-
-
     const containerStyle = {
         width: '100%',
         height: '100vh',
@@ -49,24 +41,6 @@ export default function BasePage() {
                 >
                     { /* Child components, such as markers, info windows, etc. */}
                 </GoogleMap>
-                {
-                    isLoggedIn
-                        ? (
-                            <Button className="logout_btn" variant="info" onClick={doLogout}>
-                                로그아웃
-                            </Button>
-                        )
-                        : (
-                            <>
-                                <Button className="login_btn" variant="primary" onClick={() => setModalVisibility(ModalVisibility.SHOW_LOGIN)}>
-                                    로그인
-                                </Button>
-                                <Button className="register_btn" variant="secondary" onClick={() => setModalVisibility(ModalVisibility.SHOW_REGISTER)}>
-                                    회원가입
-                                </Button>
-                            </>
-                        )
-                }
 
             </LoadScript>
         </div>
