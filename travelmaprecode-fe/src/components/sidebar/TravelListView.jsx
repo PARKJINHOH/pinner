ed7a7904883721429bd87037ef50c412b6942311
cmsong <dlcmaldmlzldxkdlrj@gmail.com>
2022-09-01 01:09:33 +0900

    [fe] raw 부트스트랩 place holder를 의미 있는 값으로 수정

--- travelmaprecode-fe/src/components/sidebar/TravelListView.jsx
+++ travelmaprecode-fe/src/components/sidebar/TravelListView.jsx
@@ -6,41 +6,32 @@ export default function TravelListView() {
             <div id='sidebar-travel-div'>
                 <li className="mb-1">
                     <button className="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="true">
-                        Home
+                        2022년 유럽여행
                     </button>
                     <div className="collapse show" id="home-collapse">
                         <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                            <li><a href="#" className="link-dark rounded">Overview</a></li>
-                            <li><a href="#" className="link-dark rounded">Updates</a></li>
-                            <li><a href="#" className="link-dark rounded">Reports</a></li>
+                            <li><a href="#" className="link-dark rounded">영국 1일차 런던</a></li>
+                            <li><a href="#" className="link-dark rounded">영국 2일차 세븐 시스터즈</a></li>
+                            <li><a href="#" className="link-dark rounded">영국 3일차 옥스포드</a></li>
                         </ul>
                     </div>
                 </li>
                 <li className="mb-1">
                     <button className="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#dashboard-collapse" aria-expanded="false">
-                        Dashboard
+                        2013년 일본 여행
                     </button>
                     <div className="collapse" id="dashboard-collapse">
                         <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                            <li><a href="#" className="link-dark rounded">Overview</a></li>
-                            <li><a href="#" className="link-dark rounded">Weekly</a></li>
-                            <li><a href="#" className="link-dark rounded">Monthly</a></li>
-                            <li><a href="#" className="link-dark rounded">Annually</a></li>
+                            <li><a href="#" className="link-dark rounded">오사카</a></li>
+                            <li><a href="#" className="link-dark rounded">교토</a></li>
+                            <li><a href="#" className="link-dark rounded">우지 녹차마을</a></li>
                         </ul>
                     </div>
                 </li>
                 <li className="mb-1">
                     <button className="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#orders-collapse" aria-expanded="false">
-                        Orders
+                        2044년 미국 여행
                     </button>
-                    <div className="collapse" id="orders-collapse">
-                        <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                            <li><a href="#" className="link-dark rounded">New</a></li>
-                            <li><a href="#" className="link-dark rounded">Processed</a></li>
-                            <li><a href="#" className="link-dark rounded">Shipped</a></li>
-                            <li><a href="#" className="link-dark rounded">Returned</a></li>
-                        </ul>
-                    </div>
                 </li>
             </div>
         </ul>
