997b2d7b35b414a57f381c9e2443a77a59074c39
cmsong <cmsong@shinasys.com>
2022-09-30 12:48:06 +0900

    [fe] 사진 업로드 추가/삭제/프리뷰 기능 추가

--- travelmaprecode-fe/src/components/modals/NewJourneyModal.jsx
+++ travelmaprecode-fe/src/components/modals/NewJourneyModal.jsx
@@ -1,62 +1,14 @@
-import { Text } from '@mantine/core';
+import { Image, SimpleGrid, Text } from '@mantine/core';
 import { Dropzone, IMAGE_MIME_TYPE } from '@mantine/dropzone';
-import React from 'react';
-import { useState } from 'react';
-import { Container, Modal } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Button, Container, Modal, Stack } from 'react-bootstrap';
 import PhotoAlbum from 'react-photo-album';
 
-import { atom, useRecoilState } from 'recoil';
 import { useAPIv1 } from '../../apis/apiv1';
 
 
-/**
- * Date를 yyyy-mm-dd 형태의 문자열로 변환
- *
- * @param {Date} date
- * @returns string
- */
-function formatDate(date) {
-    return date.toISOString().split('T')[0];
-}
-
-const now = formatDate(new Date())
-
-const newJourneyData = atom({
-    key: "newJourneyData",
-    default: {
-        date: now,
-        visible: false,
-        "geo-data": {
-            lat: 0,
-            lng: 0,
-        },
-        photos: [],
-        hashtags: [],
-    },
-});
-
-function useNewJourneyData() {
-    const [journeyData, setJourneyData] = useRecoilState(newJourneyData);
 
 
-    return {
-        setDate: (dateStr) => {
-            setJourneyData({
-                ...journeyData,
-                date: dateStr,
-            });
-        },
-    };
-}
-
-function asyncImageLoad(src) {
-    return new Promise((resolve, reject) => {
-        let img = new Image()
-        img.onload = () => resolve(img)
-        img.onerror = reject
-        img.src = src
-    })
-}
 
 /**
  * 1. 사진 업로드
@@ -67,59 +19,79 @@ function asyncImageLoad(src) {
  * @returns 
  */
 function NewJourneyModal({ travelId }) {
+    // Utils
+    /**
+     * Date를 yyyy-mm-dd 형태의 문자열로 변환
+     *
+     * @param {Date} date
+     * @returns string
+     */
+    function formatDate(date) {
+        return date.toISOString().split('T')[0];
+    }
+
+    const now = formatDate(new Date())
+
     const apiv1 = useAPIv1();
-    const [files, setFiles] = useState([]);
-    const useJourneyData = useNewJourneyData();
 
-    const previews =  files.map(async (file, index) => {
-        console.log(file);
-        const imageUrl = URL.createObjectURL(file);
-        console.log(imageUrl);
+    // 이미지 포스팅
+    const [images, _setImages] = useState([]);
+    const addImages = (imgs) => _setImages([...images, ...imgs]);
+    const removeImages = (idx) => _setImages([...images.slice(0, idx), ...images.slice(idx + 1, images.length)]);
 
-        const image = await asyncImageLoad(imageUrl);
-        console.log(image.width, image.height);
+    const [previews, setPreviews] = useState([]);
 
-        return (
-            {
-                src: imageUrl,
-                width: image.width,
-                height: image.height,
-            }
-        );
-    });
+    useEffect(() => {
+        async function imageToPreview(file, index) {
+            const imageUrl = URL.createObjectURL(file);
+            const hoverBtnStyle = {
+                position: "relative",
+                top: -40,
+                left: 10,
+            };
 
-    const [journeyData, setJourneyData] = useRecoilState(newJourneyData);
+            return <div key={imageUrl}>
+                <Image src={imageUrl} />
+                <Button style={hoverBtnStyle} variant="danger" size="sm" onClick={() => removeImages(index)}>삭제</Button>
+            </div>;
+        }
+
+        Promise.all(images.map(imageToPreview)).then(setPreviews);
+    }, [images]);
 
     /**
      * @param {File} file
      */
-    async function upload(file) {
+    async function uploadImage(file) {
         const formData = new FormData();
         formData.append("image", file);
 
-        try {
-            const resp = (await fetch("/images", {
-                method: "post",
-                headers: {
-                    "Content-Type": "multipart/form-data",
-                },
-            }));
+        const resp = (await fetch("/images", {
+            method: "post",
+            headers: {
+                "Content-Type": "multipart/form-data",
+            },
+        }));
 
-            const imageId = resp.links;
-            return imageId;
-        } catch (error) {
-            alert(error);
-        }
+        const imageId = resp.links;
+        return imageId;
     }
+
+    // Journey 데이터
+    const [date, setDate] = useState(now);
+
+
 
     /**
      * 1. POST 요청
      * 2. 응답결과가 정상일 경우
-     * 3. 
      */
-    async function onNewJourney() {
+    async function onCreate() {
+        // 사진 업로드
+        const photoIds = await Promise.all(images.map(uploadImage));
+
         // FIXME: 데이터 맞게 변환
-        await apiv1.post(`/travel/${travelId}/journey`, journeyData);
+        // await apiv1.post(`/travel/${travelId}/journey`, journeyData);
         // journeyData
     }
 
@@ -137,34 +109,55 @@ function NewJourneyModal({ travelId }) {
                     <Modal.Title id="contained-modal-title-vcenter">새로운 여행</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    {/* Location Name */}
+                    <Stack gap={5}>
+                        {/* Location Name */}
+                        <div></div>
 
-                    {/* Date Picker */}
-                    <h3>날짜</h3>
-                    <input type="date" max={now} value={journeyData.date} onChange={e => {
-                        const dateStr = e.target.value;
-                        useJourneyData.setDate(dateStr);
-                    }} />
 
-                    {/* Photos */}
-                    <h3>사진</h3>
-                    <Dropzone accept={IMAGE_MIME_TYPE} onDrop={setFiles}>
-                        <Text align="center">Drop images here</Text>
-                    </Dropzone>
-                    <PhotoAlbum layout="rows" photos={previews} />
-                    {/* <SimpleGrid
-                        cols={5}
-                        breakpoints={[{ maxWidth: 'sm', cols: 1 }]}
-                        mt={previews.length > 0 ? 'xl' : 0}
-                    >
-                        {previews}
-                    </SimpleGrid> */}
+                        {/* Date Picker */}
+                        <div>
 
-                    {/* Hash tags */}
-                    <h3>태그</h3>
+                            <h3>날짜</h3>
+                            <input type="date" max={now} value={date} onChange={e => {
+                                const dateStr = e.target.value;
+                                setDate(dateStr);
+                            }} />
+                        </div>
 
-                    {/* 저장/취소 */}
+
+                        {/* Photos */}
+                        <div>
+                            <h3>사진</h3>
+                            <PhotoAlbum layout="rows" photos={addImages} />
+                            <SimpleGrid
+                                cols={4}
+                                breakpoints={[{ maxWidth: 'sm', cols: 1 }]}
+                                mt={previews.length > 0 ? 'xl' : 0}
+                            >
+                                {[
+                                    ...previews,
+                                    <Dropzone key={"dropzone"} accept={IMAGE_MIME_TYPE} onDrop={addImages}>
+                                        <Text align="center">Drop images here</Text>
+                                    </Dropzone>
+                                ]}
+                            </SimpleGrid>
+                        </div>
+
+
+                        {/* Hash tags */}
+                        <div>
+                            <h3>태그</h3>
+
+                        </div>
+                    </Stack>
+
+
                 </Modal.Body>
+                {/* 저장/취소 */}
+                <Modal.Footer>
+                    <Button variant='outline-primary'>취소</Button>
+                    <Button variant='primary' onClick={onCreate}>저장</Button>
+                </Modal.Footer>
             </Container>
         </Modal>
     );
