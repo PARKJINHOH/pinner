b67693b6c1f00300742ddbee93898fff93780850
Jino <adg0609@naver.com>
2022-10-09 21:07:51 +0900

    h2-console 변경

--- travelmaprecode-be/src/main/java/com/example/travelmaprecodebe/config/SecurityConfig.java
+++ travelmaprecode-be/src/main/java/com/example/travelmaprecodebe/config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
                 .and()
                 .authorizeRequests()
-                    .antMatchers("/", "/login/oauth2/**", "/api/v1/traveler/**", "/h2-console/**", "/photo/**").permitAll()
+                    .antMatchers("/", "/login/oauth2/**", "/api/v1/traveler/**",  "/photo/**").permitAll()
                     .anyRequest().authenticated()
 
                 .and()
