8edb785a4f31b052acaf91ae8429829fb0e8bcd5
cmsong <dlcmaldmlzldxkdlrj@gmail.com>
2022-12-24 18:25:44 +0900

    [fe] JourneyDatePill 디자인 간소화
    Journey 날짜를 버튼에서 텍스트로 변경
    JourneyDatePill을 TravelPill에 내장
    TravelPill 리팩터링

--- travelmaprecode-fe/src/components/sidebar/TravelPill.jsx
+++ travelmaprecode-fe/src/components/sidebar/TravelPill.jsx
@@ -1,12 +1,12 @@
-import React, { useRef, useState } from 'react'
-import { Button, ButtonGroup, Dropdown, DropdownButton, Stack } from 'react-bootstrap'
-import { FiChevronRight, FiChevronDown } from 'react-icons/fi';
+import React, { useRef, useState } from 'react';
+import { Button, ButtonGroup, Dropdown, DropdownButton, Stack } from 'react-bootstrap';
+import toast from 'react-hot-toast';
 import { BsThreeDots } from 'react-icons/bs';
+import { FiChevronDown, FiChevronRight } from 'react-icons/fi';
 import { useRecoilState, useResetRecoilState, useSetRecoilState } from 'recoil';
 import { NewJourneyStep, newJourneyStepState, newLocationState } from '../../states/modal';
-import toast from 'react-hot-toast';
-import { selectedTravelIdState, selectedTravelState, travelState } from '../../states/travel';
-import JourneyDatePill from "./JourneyDatePill";
+import { selectedTravelIdState, travelState } from '../../states/travel';
+import JourneyPill from "./JourneyPill";
 
 import { useAPIv1 } from '../../apis/apiv1';
 
@@ -29,6 +29,12 @@ export default function TravelPill({ travel }) {
     const newData = uniqueDate.reduce(
         (acc, v) => [...acc, [...journeyList.filter((d) => d.date === v)]], []
     );
+
+
+    function onFoldingClick() {
+        if (collapse === true) setSelectedId(travel.id);
+        setCollapse(!collapse);
+    }
 
     const onDeleteClick = async (e) => {
         await apiv1.delete("/travel/" + travel.id)
@@ -70,59 +76,78 @@ export default function TravelPill({ travel }) {
         }
     }
 
+    // 이름 변경 시작
     function onRenameClick(e) {
         e.stopPropagation();
         setIsRenaming(true);
     }
 
+    const renameTextInput = <input type="text" autoFocus={true} onKeyDown={onKeyDownRename} onBlur={() => setIsRenaming(false)}></input>;
+
+    // 새 Journey 생성을 위해 사용자가 맵을 클릭하도록 안내
+    function onNewJourneyClick() {
+        toast((t) => (<span>
+            어디를 여행하셨나요?
+            지도를 클릭해서 Journey를 추가해요.
+        </span>));
+        setNewJourneyStep(NewJourneyStep.LOCATING);
+        setSelectedId(travel.id);
+    }
+
+
+    const iconAndTitle = <>
+        {collapse ? <FiChevronRight /> : <FiChevronDown />}
+        <div>{travel.title}</div>
+    </>;
+
     return (
         <li className="mb-2 d-grid space-between">
+            {/* Travel 버튼 */}
             <ButtonGroup>
-                <Button onClick={() => {
-                    if (collapse === true) setSelectedId(travel.id);
-
-
-                    setCollapse(!collapse);
-
-                }}>
+                <Button onClick={onFoldingClick}>
                     <Stack direction="horizontal" className='me-auto'>
-                        {
-                            isRenaming ?
-                                <input type="text" autoFocus={true} onKeyDown={onKeyDownRename} onBlur={() => setIsRenaming(false)}></input>
-                                :
-                                <>
-                                    {collapse ? <FiChevronRight /> : <FiChevronDown />}
-                                    <div>{travel.title}</div>
-                                </>
-                        }
+                        {isRenaming ? renameTextInput : iconAndTitle}
                     </Stack>
                 </Button>
-
 
                 <DropdownButton as={ButtonGroup} className='e-caret-hide hide-after' title={<BsThreeDots />}>
                     <Dropdown.Item onClick={onRenameClick}>이름 변경</Dropdown.Item>
                     <Dropdown.Item onClick={onDeleteClick}>삭제</Dropdown.Item>
-                    <Dropdown.Item onClick={() => {
-                        // 사용자가 맵을 클릭하도록 안내
-                        toast((t) => (<span>
-                            어디를 여행하셨나요?
-                            지도를 클릭해서 Journey를 추가해요.
-                        </span>));
-                        setNewJourneyStep(NewJourneyStep.LOCATING);
-                        setSelectedId(travel.id);
-                    }}>Journey 생성</Dropdown.Item>
+                    <Dropdown.Item onClick={onNewJourneyClick}>Journey 생성</Dropdown.Item>
                 </DropdownButton>
             </ButtonGroup>
 
 
+            {/* Travel 목록 */}
             {
                 collapse ||
                 <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                    {
-                        newData.map((journeys, i) => <JourneyDatePill key={i} journeys={journeys} />)
-                    }
+                    {newData.map((journeys, i) => <JourneyDatePill key={i} journeys={journeys} />)}
                 </ul>
             }
+        </li>
+    )
+}
+
+function JourneyDatePill({ journeys }) {
+
+    function drawDateTitle() {
+        return <div className='ms-3'>
+            <b key={journeys[0].id}>{journeys[0].date}</b>
+            <hr style={{ margin: 4 }}></hr>
+        </div>;
+    }
+
+    function drawJourneyPills() {
+        return journeys.map(journey => <JourneyPill key={journey.id} journey={journey} />)
+    }
+
+    return (
+        <li>
+            {drawDateTitle()}
+            <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small ms-4">
+                {drawJourneyPills()}
+            </ul>
         </li>
     )
 }
