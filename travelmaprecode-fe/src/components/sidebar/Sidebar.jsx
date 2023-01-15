627c352a7737fed505681c365629a124898f7a52
Jino <adg0609@naver.com>
2023-01-15 19:51:51 +0900

    모바일 및 상단바 제거

--- travelmaprecode-fe/src/components/sidebar/Sidebar.jsx
+++ travelmaprecode-fe/src/components/sidebar/Sidebar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Sidebar.css'
 import TravelerPill from './TravelerPill'
 import TravelListView from './TravelListView'
-import {Box, Drawer} from "@mui/material";
+import {AppBar, Box, Drawer, IconButton, Toolbar, Typography} from "@mui/material";
 import MenuIcon from '@mui/icons-material/Menu';
 
 const drawerWidth = 280;
@@ -17,48 +17,12 @@ export default function Sidebar() {
     return (
         <div className="sidebar d-flex flex-column flex-shrink-0 bg-white">
             <Box sx={{display: 'flex'}}>
-                {/*상단바(TopBar)*/}
-                {/*<AppBar
-                position="fixed"
-                sx={{
-                    width: {sm: `calc(100% - ${drawerWidth}px)`},
-                    ml: {sm: `${drawerWidth}px`},
-                }}
-            >
-                <Toolbar>
-                    <IconButton
-                        color="inherit"
-                        aria-label="open drawer"
-                        edge="start"
-                        onClick={handleDrawerToggle}
-                        sx={{mr: 2, display: {sm: 'none'}}}
-                    >
-                        <MenuIcon/>
-                    </IconButton>
-                    <Typography variant="h6" noWrap component="div">
-                        Responsive drawer
-                    </Typography>
-                </Toolbar>
-            </AppBar>*/}
-
-
                 <Box
                     component="nav"
                     sx={{width: {sm: drawerWidth}, flexShrink: {sm: 0}}}
                     aria-label="mailbox folders"
                 >
-                    <Drawer
-                        variant="temporary"
-                        open={mobileOpen}
-                        onClose={handleDrawerToggle}
-                        ModalProps={{
-                            keepMounted: true, // Better open performance on mobile.
-                        }}
-                        sx={{display: {xs: 'block', sm: 'none'}, '& .MuiDrawer-paper': {boxSizing: 'border-box', width: drawerWidth},}}
-                    >
-                    </Drawer>
-
-                    {/*사이드바(SideBar)*/}
+                    {/*사이드바(SideBar) - PC*/}
                     <Drawer
                         variant="permanent"
                         sx={{display: {xs: 'none', sm: 'block'}, '& .MuiDrawer-paper': {boxSizing: 'border-box', width: drawerWidth},}}
