cb74688bff0098c3a5fa21a9d696d05903a204f4
Jino <adg0609@naver.com>
2023-03-11 16:41:21 +0900

    MuiAlertComponent 추가

---
+++ travelmaprecode-fe/src/components/alert/AlertComponent.jsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import Alert from '@mui/material/Alert';
+import Stack from '@mui/material/Stack';
+
+function AlertComponent(props) {
+    return (
+        <Stack sx={{width: '100%'}} spacing={2}>
+            <Alert severity={props.type}>{props.message}</Alert>
+        </Stack>
+    );
+}
+
+function errorAlert(str) {
+    return <AlertComponent type="error" message={str}/>;
+}
+
+function warningAlert(str) {
+    return <AlertComponent type="warning" message={str}/>;
+}
+
+function infoAlert(str) {
+    return <AlertComponent type="info" message={str}/>;
+}
+
+function successAlert(str) {
+    return <AlertComponent type="info" message={str}/>;
+}
+
+export {errorAlert, warningAlert};
