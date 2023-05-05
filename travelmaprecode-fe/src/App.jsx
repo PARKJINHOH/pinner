1307c842ad51303324f1f7293edd53859282960d
PARK JINHOH <adg0609@naver.com>
2023-05-05 20:00:57 +0900

    [REFACTOR] Sidebar로 리펙토링 및 클릭시 selected 추가

--- travelmaprecode-fe/src/App.jsx
+++ travelmaprecode-fe/src/App.jsx
@@ -2,86 +2,21 @@ import './style/globals.css';
 import {RecoilRoot} from 'recoil';
 import React from 'react';
 import BasePage from './pages/BasePage';
-import Sidebar from './components/sidebar/Sidebar';
-
-import MapIcon from '@mui/icons-material/Map';
-import LanguageIcon from '@mui/icons-material/Language';
-import {AppBar, Box, Button, CssBaseline, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography} from "@mui/material";
-import {Avatar} from "@mantine/core";
-import Stack from "@mui/material/Stack";
-import IconButton from "@mui/material/IconButton";
-
-const drawerWidth = 70; // 사이드바 너비
-
-// Style
-const listItemButtonStyles = {
-    minHeight: 40, // Icon 간격
-    justifyContent: 'center',
-    flexDirection: 'column', // 세로 방향으로 표시
-};
-
-const listItemIconStyles = {
-    justifyContent: 'center',
-};
-
-//! Style
+import { Box,  CssBaseline} from "@mui/material";
+import Sidebar from "./components/sidebar/Sidebar";
 
 function App() {
     return (
         <RecoilRoot>
             <Box sx={{display: 'flex'}}>
                 <CssBaseline/>
-                <Drawer
-                    sx={{
-                        width: drawerWidth,
-                        flexShrink: 0,
-                        '& .MuiDrawer-paper': {
-                            width: drawerWidth,
-                            boxSizing: 'border-box',
-                        },
-                    }}
-                    variant="permanent"
-                    anchor="left"
-                >
-                    <List>
-                        <ListItem disablePadding>
-                            <ListItemButton sx={listItemButtonStyles}>
-                                <ListItemIcon sx={listItemIconStyles}>
-                                    <MapIcon sx={{fontSize: 30}}/>
-                                </ListItemIcon>
-                            </ListItemButton>
-                        </ListItem>
-                        <ListItem disablePadding>
-                            <ListItemButton sx={listItemButtonStyles}>
-                                <ListItemIcon sx={listItemIconStyles}>
-                                    <LanguageIcon sx={{fontSize: 30}}/>
-                                </ListItemIcon>
-                                <ListItemText
-                                    primary={
-                                        <Typography variant="subtitle2" noWrap>
-                                            둘러보기
-                                        </Typography>
-                                    }
-                                />
-                            </ListItemButton>
-                        </ListItem>
-                    </List>
-                    <Button
-                        sx={{marginTop: 'auto'}}
-                    >
-                        <Avatar sx={{
-                            width: 56, height: 56,
-                        }}/>
-                    </Button>
-                </Drawer>
+                    <Sidebar/>
                 <Box
                     sx={{flexGrow: 1}}
                 >
                     <BasePage/>
                 </Box>
             </Box>
-
-            {/*<Sidebar />*/}
         </RecoilRoot>
     );
 }
--- travelmaprecode-fe/src/components/sidebar/Sidebar.jsx
+++ travelmaprecode-fe/src/components/sidebar/Sidebar.jsx
@@ -1,42 +1,90 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Sidebar.css'
 import TravelerPill from './TravelerPill'
 import TravelListView from './TravelListView'
-import { Box, Divider, Drawer } from "@mui/material";
+import {Box, Button, Divider, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography} from "@mui/material";
+import MapIcon from "@mui/icons-material/Map";
+import LanguageIcon from "@mui/icons-material/Language";
+import {Avatar} from "@mantine/core";
 
-const drawerWidth = 280;
+
+const drawerWidth = 70; // 사이드바 너비
+
+// Style
+const listItemButtonStyles = {
+    minHeight: 40, // Icon 간격
+    justifyContent: 'center',
+    flexDirection: 'column', // 세로 방향으로 표시
+};
+
+const listItemIconStyles = {
+    justifyContent: 'center',
+};
+
+//! Style
 
 export default function Sidebar() {
-    const [mobileOpen, setMobileOpen] = React.useState(false);
-    const handleDrawerToggle = () => {
-        setMobileOpen(!mobileOpen);
+    const [selectedItem, setSelectedItem] = useState(null);
+
+    const handleItemClick = (index) => {
+        if (selectedItem !== null && selectedItem === index) {
+            setSelectedItem(null);
+            return;
+        }
+        setSelectedItem(index);
     };
 
-
     return (
-        <div>
-            <Box
-                component="nav"
-                sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
-                aria-label="mailbox folders"
+        <Drawer
+            sx={{
+                width: drawerWidth,
+                flexShrink: 0,
+                '& .MuiDrawer-paper': {
+                    width: drawerWidth,
+                    boxSizing: 'border-box',
+                },
+            }}
+            variant="permanent"
+            anchor="left"
+        >
+            <List>
+                <ListItem disablePadding>
+                    <ListItemButton
+                        selected={selectedItem === 0}
+                        sx={listItemButtonStyles}
+                        onClick={() => handleItemClick(0)}
+                    >
+                        <ListItemIcon sx={listItemIconStyles}>
+                            <MapIcon sx={{fontSize: 30}}/>
+                        </ListItemIcon>
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton
+                        selected={selectedItem === 1}
+                        sx={listItemButtonStyles}
+                        onClick={() => handleItemClick(1)}
+                    >
+                        <ListItemIcon sx={listItemIconStyles}>
+                            <LanguageIcon sx={{fontSize: 30}}/>
+                        </ListItemIcon>
+                        <ListItemText
+                            primary={
+                                <Typography variant="subtitle2" noWrap>
+                                    둘러보기
+                                </Typography>
+                            }
+                        />
+                    </ListItemButton>
+                </ListItem>
+            </List>
+            <Button
+                sx={{marginTop: 'auto'}}
             >
-                {/*사이드바(SideBar) - PC*/}
-                <Drawer
-                    variant="permanent"
-                    sx={{ display: { xs: 'none', sm: 'block' }, '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth }, }}
-                    open
-                >
-                    <Box sx={{ padding: 2, fontSize: 24, fontWeight: 'bold', textAlign: 'center' }}>
-                        Travel Map Record
-                    </Box>
-
-                    <Divider />
-
-                    <TravelListView />
-
-                    <TravelerPill />
-                </Drawer>
-            </Box>
-        </div >
+                <Avatar sx={{
+                    width: 56, height: 56,
+                }}/>
+            </Button>
+        </Drawer>
     )
 }
