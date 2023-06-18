cdd1410bbf712c2879545aced52f3c2acb652f50
PARK JINHOH <adg0609@naver.com>
2023-06-18 18:22:25 +0900

    [Add] Travel Title 수정기능 추가

--- travelmaprecode-fe/src/components/sidebar/JourneyPill.jsx
+++ travelmaprecode-fe/src/components/sidebar/JourneyPill.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-import {Box, Container, Typography} from "@mui/material";
+import {useAPIv1} from '../../apis/apiv1'
+
+import {Box, Container, TextField, Typography} from "@mui/material";
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import './JourneyPill.css';
 import AddIcon from "@mui/icons-material/Add";
+
+import CreateIcon from '@mui/icons-material/Create';
+import {useSetRecoilState} from "recoil";
+import {travelState} from "../../states/travel";
 
 /**
  * Journey 상세 정보를 보여주는 컴포넌트
@@ -11,6 +17,39 @@ import AddIcon from "@mui/icons-material/Add";
  */
 export default function JourneyPill({ travel }) {
     console.log('travel', travel);
+    const apiv1 = useAPIv1();
+
+    const setTravels = useSetRecoilState(travelState);
+
+    const[isTitleEditing, setIsTitleEditing] = useState(false);
+    const[isDateEditing, setIsDateEditing] = useState(false);
+
+    /**
+     * 이름 변경 중 ESC키를 누르면 취소를, 엔터를 누르면 적용한다.
+     * @param {KeyboardEvent} e
+     */
+    async function onKeyDownRename(e) {
+        const isEsc = e.key === "Escape";
+        const isEnter = e.key === "Enter";
+
+        if (isEsc || isEnter) {
+            e.preventDefault();
+            if (isEnter) {
+                const titleJson = JSON.stringify({
+                    title: e.target.value,
+                });
+
+                await apiv1.patch("/travel/" + travel.id, titleJson)
+                    .then((response) => {
+                        if (response.status === 200) {
+                            setTravels(response.data);
+                        }
+                    });
+
+            }
+            setIsTitleEditing(false);
+        }
+    }
 
     return (
         // Todo : 보기 <> 편집(Edit) 변경될 때 UI 변경해야함.
@@ -29,9 +68,28 @@ export default function JourneyPill({ travel }) {
 
             {/* 타이틀 영역 */}
             <div align="center" className="journey-title">
-                <Typography variant='h5'>
-                    {travel.title}
-                </Typography>
+                {
+                    isTitleEditing ?
+                        <>
+                            <TextField
+                                id="travel-title"
+                                defaultValue={travel.title}
+                                hiddenLabel
+                                variant="standard"
+                                onKeyDown={onKeyDownRename}
+                                onBlur={() => setIsTitleEditing(false)}
+                            />
+                        </>
+                        :
+                        <>
+                            <Typography variant='h5'>
+                                {travel.title}
+                                <CreateIcon
+                                    onClick={() => setIsTitleEditing(true)}
+                                />
+                            </Typography>
+                        </>
+                }
                 <Typography variant='subtitle1'>
                     {/*{travel.startDate} ~ {travel.endDate}*/}
                     2023.01.01 ~ 2023.12.31
