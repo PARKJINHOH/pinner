5a87b5e9fd4c35242b7348eee76fbaf8bf3ff414
PARK JINHOH <adg0609@naver.com>
2023-06-20 23:11:56 +0900

    [Refator] 폴더명 변경

--- travelmaprecode-fe/src/App.jsx
+++ travelmaprecode-fe/src/App.jsx
@@ -3,7 +3,7 @@ import {RecoilRoot} from 'recoil';
 import React from 'react';
 import BasePage from './pages/BasePage';
 import { Box,  CssBaseline} from "@mui/material";
-import Sidebar from "./components/sidebar/Sidebar";
+import Sidebar from "./components/panel/Sidebar";
 
 function App() {
     return (
