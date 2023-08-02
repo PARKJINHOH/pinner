16049aa97a8cb900eb9beee1857b80a971b8be1c
PARK JINHOH <adg0609@naver.com>
2023-08-02 16:21:31 +0900

    [del] 사용하지 않는 photo 삭제

--- travelmaprecode-be/src/main/java/com/example/travelmaprecodebe/photo/PhotoModifier.java
+++
@@ -1,6 +0,0 @@
-package com.example.travelmaprecodebe.photo;
-
-public interface PhotoModifier<Photo> {
-    Photo resize(Photo photo, int height, int width);
-    Photo format(Photo photo, PhotoFormat format);
-}
