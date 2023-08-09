06996d7e030e3149eca714539e710c583c20a27e
cmsong <cmsong@shinasys.com>
2023-08-09 11:06:34 +0900

    [Add] 사진/위치정보 타입 업데이트

--- travelmaprecode-fe/src/apis/types.js
+++ travelmaprecode-fe/src/apis/types.js
@@ -12,8 +12,17 @@
 * @property {number} orderKey
 * @property {string} date
 * @property {string[]} hashtags
-* @property {string[]} photos
+* @property {Photo[]} photos
 * @property {geoLocationDto} geoLocationDto
+*/
+
+/**
+* @typedef {object} Photo
+ * @property {number} id
+ * @property {string} src
+ * @property {number} fileSize
+ * @property {number} width
+ * @property {number} height
 */
 
 /**
@@ -21,6 +30,7 @@
 * @property {number} lat
 * @property {number} lng
 * @property {string} name
+ * @property {string} countryCd
 */
 
 /**
