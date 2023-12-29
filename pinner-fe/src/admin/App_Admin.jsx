fcb5d413febbd93e4f48b54add9b67c001f4b990
PARK JINHOH <adg0609@naver.com>
2023-12-29 16:06:20 +0900

    [add] admin login page 프론트 구현

--- pinner-fe/src/admin/App_Admin.jsx
+++ pinner-fe/src/admin/App_Admin.jsx
@@ -1,9 +1,97 @@
-import React from 'react';
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
 
 export default function App_Admin() {
+    function handleSubmit() {
+
+    }
+
     return (
-        <>
-            admin
-        </>
+        <Box justifyContent="center" alignItems="center" sx={{ display: 'flex', width: 'auto', height: '100vh', bgcolor: '#EEF2F6'}}>
+            <Container component="main" maxWidth="xs" sx={{bgcolor: '#ffffff', borderRadius: '15px'}}>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        marginTop: '50px',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{m: 1, bgcolor: 'secondary.main'}}>
+                        <LockOutlinedIcon/>
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Admin
+                    </Typography>
+                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{mt: 1}}>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                        />
+                        <FormControlLabel
+                            control={<Checkbox value="remember" color="primary"/>}
+                            label="Remember me"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{mt: 3, mb: 2}}
+                        >
+                            Sign In
+                        </Button>
+                        <Grid container>
+                            <Grid item xs>
+                                <Link href="#" variant="body2">
+                                    Forgot password?
+                                </Link>
+                            </Grid>
+                            {/*<Grid item>*/}
+                            {/*    <Link href="#" variant="body2">*/}
+                            {/*        {"Don't have an account? Sign Up"}*/}
+                            {/*    </Link>*/}
+                            {/*</Grid>*/}
+                        </Grid>
+                    </Box>
+                </Box>
+                <Box sx={{mt: 8, mb: 4}}>
+                    <Typography variant="body2" color="text.secondary" align="center">
+                        {'Copyright © '}
+                        <Link color="inherit" href="https://pinner.dev">
+                            https://pinner.dev
+                        </Link>{' '}
+                        {new Date().getFullYear()}
+                        {'.'}
+                    </Typography>
+                </Box>
+            </Container>
+        </Box>
     );
 }
