fc7300ff6d45d7d9e4b4bd531b6a8912d5949140
PARK JINHOH <adg0609@naver.com>
2023-12-29 14:45:28 +0900

    [add] admin 페이지 시작

--- pinner-fe/src/index.jsx
+++ pinner-fe/src/index.jsx
@@ -1,13 +1,20 @@
-import AfterOAuthHandler from 'pages/AfterOAuthHandler';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+// component
 import App from 'App';
+import App_Admin from 'admin/App_Admin';
+import AfterOAuthHandler from 'pages/AfterOAuthHandler';
 
 const router = createBrowserRouter([
     {
       path: "/",
       element: <App />,
+    },
+    {
+        path: "/admin",
+        element: <App_Admin />,
     },
     {
       path: "/afteroauth",
