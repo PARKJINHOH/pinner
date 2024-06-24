a2dbb27f73a1cc5e052fcde5f1bc3405413c8b90
PARK JINHOH <adg0609@naver.com>
2024-06-24 17:17:40 +0900

    [fix] 로고 추가

--- pinner-fe/src/components/panel/community/navbar/Navbar.jsx
+++ pinner-fe/src/components/panel/community/navbar/Navbar.jsx
@@ -8,23 +8,30 @@ import {Typography} from "@mui/material";
 
 // css
 import style from './Navbar.module.css';
+import logo from "assets/images/pinner_logo.png";
 
 export default function Navbar() {
 
     return (
         <div className={style.wrap}>
             <div className={style.menu}>
+                <img
+                    src={logo}
+                    alt={logo}
+                    className={style.logo}
+                    loading="lazy"
+                />
                 <div className={style.menu_item}>
-                    <Typography variant="subtitle1" sx={{ fontWeight: 'bold' }}>공지사항</Typography>
+                    <Typography variant="subtitle1" sx={{fontWeight: 'bold'}}>공지사항</Typography>
                 </div>
                 <div className={style.menu_item}>
-                    <Typography variant="subtitle1" sx={{ fontWeight: 'bold' }}>커뮤니티</Typography>
+                    <Typography variant="subtitle1" sx={{fontWeight: 'bold'}}>커뮤니티</Typography>
                 </div>
                 <div className={style.menu_item}>
-                    <Typography variant="subtitle1" sx={{ fontWeight: 'bold' }}>추천 여행사</Typography>
+                    <Typography variant="subtitle1" sx={{fontWeight: 'bold'}}>추천 여행사</Typography>
                 </div>
                 <div className={style.menu_item}>
-                    <Typography variant="subtitle1" sx={{ fontWeight: 'bold' }}>Q&A</Typography>
+                    <Typography variant="subtitle1" sx={{fontWeight: 'bold'}}>Q&A</Typography>
                 </div>
             </div>
             <Divider/>
